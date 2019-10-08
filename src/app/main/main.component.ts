027d2243515108250a453200c2439b3f86e059ea
wayne9499 <dheerajsharma9499@gmail.com>
2019-10-08 12:26:46 +0530

    🏃 3rd commit

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  d = new Date();
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  public ngOnInit() {       // JQuery
+    const action = 'click';
+    const speed = '500';
 
+    $(document).ready(() =>  {
+      $('li.q').on(action , function() {
+        $(this).next()
+          .slideToggle(speed)
+            .siblings('li.a')
+              .slideUp();
+        const img = $(this).children('img');
+        $('img').not(img).removeClass('rotate');
+        img.toggleClass('rotate');
+      });
+    });
+  }
 }
